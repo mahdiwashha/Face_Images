0e6c66acf9cc18a13db01c7c1f902e7e8beee80c
mahdiwashha <31579222+mahdiwashha@users.noreply.github.com>
2017-09-19 01:23:19 +0200

    first commit

--- scc.sh
+++ scc.sh
@@ -1,11 +1,25 @@
 #!/bin/bash
+per=0;
 for entry in "/Users/mahdi/Desktop/mahmoud_pics"/*
 do
+val=per;
   mv $entry "/Users/mahdi/Downloads/mahhhh"
-  git init
-  git add .
-  git commit -m "first commit"
-  git push -u origin master
-  echo $entry
+
+if (( $per % 200 == 0 ))           # no need for brackets
+then
+git init
+git add .
+git commit -m "first commit"
+git push -u origin master
+fi
+let "val++";
+per=$val;
+echo $entry
 done
+
+git init
+git add .
+git commit -m "first commit"
+git push -u origin master
+
 
